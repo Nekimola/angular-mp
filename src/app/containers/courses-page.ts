fecccde999b779dccd640ee33bae0f7e4762ffb0
Oleg <margin2call@mail.ru>
2017-03-19 17:12:27 +0200

    Show spinner on courses loading

--- src/app/containers/courses-page.ts
+++ src/app/containers/courses-page.ts
@@ -12,18 +12,23 @@ import { AppState } from "../store";
   template: `    
     <section class="content">
       <tool-bar (search)="onSearch($event)"></tool-bar>
+      <img src="/public/images/spinner.gif" 
+           class="spinner" 
+           *ngIf="isLoading$ | async" alt="">
       <courses-list [courses]="courses$ | async" (remove)="onRemove($event)"></courses-list>
     </section>
   `
 })
 export class CoursesPageComponent implements OnInit {
   courses$: Observable<Course[]>;
+  isLoading$: Observable<boolean>;
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit () {
     this.store.dispatch(new LoadCoursesAction());
     this.courses$ = this.store.select(appState => appState.courses.entities);
+    this.isLoading$ = this.store.select(appState => appState.courses.loading);
   }
 
   onRemove (id) {
